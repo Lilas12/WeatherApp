ca42ab6fa9711626a50245f654e481622560318d
Lilas Alnesr <lelas.alneser@gmail.com>
2026-05-30 02:33:27 +0200

    fix: update package.json and vite.config.js for deployment

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/js-react";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/WeatherApp/", // <-- Lägg till exakt denna rad!
+  base: "/WeatherApp/",
 });
