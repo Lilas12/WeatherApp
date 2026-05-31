61a04149bc300b3c6db2b3a27c5c152fbedd7361
Lilas Alnesr <lelas.alneser@gmail.com>
2026-05-31 21:53:07 +0200

    delete styled components from App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,10 @@
-import styled, { createGlobalStyle } from 'styled-components';
 import Weather from './Weather.jsx';
-
-
-const GlobalStyle = createGlobalStyle`
-  body {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-    background-color: #f0f4f8;
-    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    min-height: 100vh;
-  }
-`;
-
-const AppContainer = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 100%;
-  padding: 20px;
-`;
 
 function App() {
   return (
     <>
-      <GlobalStyle />
-      <AppContainer>
-        <Weather />
-      </AppContainer>
+      {/* Vi renderar bara Weather-komponenten eftersom den sköter sin egen bakgrund och stil! */}
+      <Weather />
     </>
   );
 }
